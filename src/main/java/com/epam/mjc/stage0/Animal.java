a7935f9b522f4e35ae62c258e9105e90f340abdb
Nata Braun <pushistik2030@gmail.com>
2023-02-12 21:45:07 +0300

    Solved

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,54 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) throws IllegalArgumentException {
+        if (color == null ) {
+            throw new IllegalArgumentException();
+        }
+        if (numberOfPaws<=0){
+            throw new IllegalArgumentException();
+        }
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public String getDescription(){
+        String patternAnimals;
+        String paws;
+        String fur;
+        if (numberOfPaws==1) {
+            paws = Integer.toString(numberOfPaws) + " paw";
+        }else{
+            paws = Integer.toString(numberOfPaws) + " paws";
+        }
+        if (hasFur == true) {
+            fur = "a fur";
+        }else{
+            fur = "no fur";
+        }
+        patternAnimals = "This animal is mostly " + color + ". It has " + paws + " and " + fur +".";
+        return patternAnimals;
+
+
+    }
 }
+
+
+
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,35 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+
+
+
+    public Bird() throws IllegalArgumentException {
+        super("blue", 2, false);
+        getDescription();
+    }
+
+
+    @Override
+    public String getDescription() {
+        String patternAnimals;
+        String paws;
+        String fur;
+        if (getNumberOfPaws() == 1) {
+            paws = Integer.toString(getNumberOfPaws()) + " paw";
+        } else {
+            paws = Integer.toString(getNumberOfPaws()) + " paws";
+        }
+        if (isHasFur()) {
+            fur = "a fur";
+        } else {
+            fur = "no fur";
+        }
+        patternAnimals = "This animal is mostly " + getColor() + ". It has " + paws + " and " + fur + ". Moreover, it has 2 wings and can fly.";
+        System.out.println(patternAnimals);
+        return patternAnimals;
+    }
+
+
 
 }
